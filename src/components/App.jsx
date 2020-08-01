7100844c4f8af77e944038372a16511e5112f10f
SnehaCherian <snehaacherian@gmail.com>
2020-08-01 15:05:56 +0530

    part 2

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+//function createNote(note){ ////////used arrow function instead/////
+  // return <Note key={note.id} title={note.title} content={note.content} />;
+//}
+
+function App(){
+   return( <div><Header />
+     {notes.map(note =><Note key={note.id} title={note.title} content={note.content} />)}
+    <Footer />
+    </div>) ;
+}
+
+export default App;
